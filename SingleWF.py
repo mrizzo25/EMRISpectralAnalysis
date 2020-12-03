fa4bf9feb20020893834a1286c4ffd83cd234ca9
mrizzo25 <monriz5@aol.com>
2020-12-03 03:26:29 -0600

    bug fixes to make module independently usable

--- SingleWF.py
+++ SingleWF.py
@@ -12,64 +12,15 @@ import argparse
 
 plt.rcParams.update({'font.size': 16})
 
-#12 hours of data with default settings
-dt = 60*60*12/2000000
-
-#default parameters 
-params = {'backint': False, #end @ plunge and integrate backward
-        'LISA': False, #convert to LISA response (Cutler 1998)
-        'length': 2000000, #waveform points
-        'dt': dt, #time step (s)
-        'p': 7.0, #initial semilatus rectum
-        'T': 1.166, #waveform dutation (yrs) *if dt unspecified
-        'f': 2.e-3, #reference GW freq (Hz) *if p unspecified
-        'T_fit': 1., #max duration of local fit (??)
-        'mu': 1.e1, #compact object mass (M_sun)
-        'M': 1.e6, #BH mass (M_sun)
-        's': 0.8, #BH spin (a/M)
-        'e': 0.0001, #initial eccentricity
-        'iota': 0.0, #inclination angle of L from S
-        'gamma': 0., #Initial angle of periapsis from LxS (?)
-        'psi': 0., #Initial true anomoly (?)
-        'theta_S': 0.785, #source polar angle in ecliptic coords (?)
-        'phi_S': 0.785, #source azimuthal angle in ecliptiv coords (?)
-        'theta_K': 1.05, #BH spin polar angle
-        'phi_K': 1.05, #BH spin azimuthal angle
-        'alpha': 0., #initial azimuthal orientation (Eq.(18) in Barack & Cutler, 2004)
-        'D': 1.} #source distance (Gpc)
-
-
-
-param_type_dict = {'backint': bool,
-        'LISA': bool,
-        'length': int,
-        'dt': float,
-        'p': float,
-        'T': float,
-        'f': float,
-        'T_fit': float,
-        'mu': float,
-        'M': float,
-        's': float,
-        'e': float,
-        'iota': float,
-        'gamma': float,
-        'psi': float,
-        'theta_S': float,
-        'phi_S': float,
-        'theta_K': float,
-        'phi_K': float,
-        'alpha': float,
-        'D': float}
 
 class GenerateSingleSpectrum(object):
 
-    def __main__(self, args):
+    def __init__(self, args):
         """
         Initialized with dictionary of default params and 
         """
         
-        self.params = params
+        self.params = self.__default_params()
 
         self.save_file = args.save_file
         self.save_fig = args.save_fig
@@ -96,7 +47,7 @@ class GenerateSingleSpectrum(object):
                     use_cl=self.use_cl, angle_avg=self.angle_avg)
         else:
             #no angle avg
-            self.ds = DiscretizeSpectra(params, fname=self.fname, \
+            self.ds = DiscretizeSpectra(self.params, fname=self.fname, \
                     use_cl=self.use_cl)
         
         #output aak phases 
@@ -116,7 +67,38 @@ class GenerateSingleSpectrum(object):
             if 'data' not in os.listdir("./"):
                 os.mkdir('data')
 
-            self.save_file()
+            self.save_data()
+
+    def __default_params(self):
+        """
+        Defines and returns default parameters when initializing
+        """
+
+        #default parameters 
+        params = {'backint': False, #end @ plunge and integrate backward
+                'LISA': False, #convert to LISA response (Cutler 1998)
+                'length': 2000000, #waveform points
+                'dt': 0.0216, #time step (s)
+                'p': 7.0, #initial semilatus rectum
+                'T': 1.166, #waveform dutation (yrs) *if dt unspecified
+                'f': 2.e-3, #reference GW freq (Hz) *if p unspecified
+                'T_fit': 1., #max duration of local fit (??)
+                'mu': 1.e1, #compact object mass (M_sun)
+                'M': 1.e6, #BH mass (M_sun)
+                's': 0.8, #BH spin (a/M)
+                'e': 0.0001, #initial eccentricity
+                'iota': 0.0, #inclination angle of L from S
+                'gamma': 0., #Initial angle of periapsis from LxS (?)
+                'psi': 0., #Initial true anomoly (?)
+                'theta_S': 0.785, #source polar angle in ecliptic coords (?)
+                'phi_S': 0.785, #source azimuthal angle in ecliptiv coords (?)
+                'theta_K': 1.05, #BH spin polar angle
+                'phi_K': 1.05, #BH spin azimuthal angle
+                'alpha': 0., #initial azimuthal orientation (Eq.(18) in Barack & Cutler, 2004)
+                'D': 1.} #source distance (Gpc)
+
+        return params
+
 
     def plot_wf(self):
         """
@@ -124,7 +106,6 @@ class GenerateSingleSpectrum(object):
         """
 
         #plot waveforms
-        #should really modify this to return a figure...
         fig, axs = plt.subplots(2, figsize=(10, 6), sharex=True)
         axs[0].set_title("GW Strain")
         axs[0].plot(self.ds.t, self.ds.h_plus)
@@ -133,7 +114,7 @@ class GenerateSingleSpectrum(object):
         axs[1].plot(self.ds.t, self.ds.h_cross)
         axs[1].set_ylabel("$h_x$")
         axs[1].set_xlabel("t")
-        plt.tight_layout()
+        fig.tight_layout()
 
         #save or show
         if self.save_fig:
@@ -146,7 +127,7 @@ class GenerateSingleSpectrum(object):
         Plot generated spectrum
         """
         #make plot for positive frequency data
-        mask = ds.freqs > 1e-16
+        mask = self.ds.freqs > 1e-16
 
         fig = plt.figure(figsize=(10, 6))
         plt.title("Power Spectrum")
@@ -166,7 +147,7 @@ class GenerateSingleSpectrum(object):
         else:
             return fig
     
-    def save_file(self):
+    def save_data(self):
         """
         Save data to file
         """
@@ -195,7 +176,7 @@ class GenerateSingleSpectrum(object):
             #create and populate h5 datasets
             for p in output_params:
 
-                exec("f_wf.create_dataset('{}', data = params['{}'])"\
+                exec("f_wf.create_dataset('{}', data = self.params['{}'])"\
                     .format(p, p, p))
 
             f_wf.create_dataset('h_plus', data = self.ds.h_plus)
@@ -235,38 +216,65 @@ class GenerateSingleSpectrum(object):
 
 
 
+def parser():
+    """
+    Argument parsing
+    """
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--save-file", action='store_true', help="save data to file or not")
-parser.add_argument("--save-fig", action='store_true', help="save strain and spectra plots")
-parser.add_argument("--fname", type=str, help='filename of output')
-parser.add_argument("--use-cl", action='store_true', help="generate wf using command line")
-parser.add_argument("--angle-avg", action='store_true')
-parser.add_argument("--output-phases", action='store_true', help='save phase data along with waveform data')
-parser.add_argument("--scatter", action='store_true', help="generate spectra scatter plot")
-parser.add_argument("--overwrite", action='store_true', help="if data file exists, overwrite")
-parsed, unknown = parser.parse_known_args()
+    #dictionary of AAK wrapper parameter types
+    param_type_dict = {'backint': bool,
+        'LISA': bool,
+        'length': int,
+        'dt': float,
+        'p': float,
+        'T': float,
+        'f': float,
+        'T_fit': float,
+        'mu': float,
+        'M': float,
+        's': float,
+        'e': float,
+        'iota': float,
+        'gamma': float,
+        'psi': float,
+        'theta_S': float,
+        'phi_S': float,
+        'theta_K': float,
+        'phi_K': float,
+        'alpha': float,
+        'D': float}
 
-#accept additional params as emri params
-for arg in unknown:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save-file", action='store_true', help="save data to file or not")
+    parser.add_argument("--save-fig", action='store_true', help="save strain and spectra plots")
+    parser.add_argument("--fname", type=str, help='filename of output')
+    parser.add_argument("--use-cl", action='store_true', help="generate wf using command line")
+    parser.add_argument("--angle-avg", action='store_true')
+    parser.add_argument("--output-phases", action='store_true', help='save phase data along with waveform data')
+    parser.add_argument("--scatter", action='store_true', help="generate spectra scatter plot")
+    parser.add_argument("--overwrite", action='store_true', help="if data file exists, overwrite")
+    parsed, unknown = parser.parse_known_args()
 
-    if arg.startswith(("--", "-")) and (arg.replace('--', '') in params.keys() or \
-            arg.replace('-', '') in params.keys()):
+    #accept additional params as AAK EMRI parameters 
+    for arg in unknown:
 
-        if arg.startswith("--"):
-            key = arg.replace('--', '')
-        else:
-            key = arg.replace('-', '')
+        if arg.startswith(("--", "-")) and (arg.replace('--', '') in param_type_dict.keys() or \
+                arg.replace('-', '') in param_type_dict.keys()):
 
-        parser.add_argument(arg, type=param_type_dict[key])
+            if arg.startswith("--"):
+                key = arg.replace('--', '')
+            else:
+                key = arg.replace('-', '')
 
-args = parser.parse_args()
+            parser.add_argument(arg, type=param_type_dict[key])
+
+    args = parser.parse_args()
+    return args
 
 #############Script################
 
-GenerateSingleSpectrum(args)
+if __name__ == "__main__":
 
-
-
-
+    args = parser()
+    GenerateSingleSpectrum(args)
 
